f7e1222ac23a9551ee15558cc0773906d766e36d
lekhrajdinkar <lekhrajdinkar@gmail.com>
2025-06-22 17:51:46 -0700

    resume builder

--- docs/resume-builder/build2.py
+++ docs/resume-builder/build2.py
@@ -7,13 +7,14 @@ TEMPLATE_DIR = "template"; YAML_DIR = "yaml"
 
 all_templates = [f.replace(".html", "").split('-')[1] for f in os.listdir(TEMPLATE_DIR) if f.startswith(f"template")]
 print("template options - ", *all_templates)
-template1=input("choose template : ")
+template1=input("choose template [default : 2] : ") or '2'
 if template1 not in [*all_templates]:  yaml1 = all_templates[0]; print(f"Invalid yaml option provided. Defaulting to {template1}.")
 
 all_yaml = [f.replace(".yaml", "").split('-')[1] for f in os.listdir(YAML_DIR) if f.startswith(f"resume")]
 print("yaml options - ", *all_yaml)
-yaml1=input("choose yaml : ")
+yaml1=input("choose yaml [default : se3] : ") or 'se3'
 if yaml1 not in [*all_yaml]:  yaml1 = all_yaml[0]; print(f"Invalid yaml option provided. Defaulting to {yaml1}.")
+
 
 TEMPLATE_FILE = f"template/template-{template1}.html"
 YAML_FILE = f"yaml/resume-{yaml1}.yaml"
@@ -37,7 +38,13 @@ def next_version():
             versions.append(int(parts[1]))
     return max(versions, default=0) + 1
 
-version = next_version()
+
+version_option=input("enter version option [latest (hit enter)  | increment (press i)]: ") or 'latest'
+if version_option == 'latest':
+    version =  'latest'
+else:
+    version = next_version()
+
 html_file = f"resume-from-tpl-{template1}-profile-{yaml1}_v{version}.html" #f"{BASE_NAME}_v{version}.html"
 pdf_file = f"resume-from-tpl-{template1}-profile-{yaml1}_v{version}.pdf" #f"{BASE_NAME}_v{version}.pdf"
 
@@ -59,5 +66,5 @@ with open(html_path, "w", encoding="utf-8") as f:
 pdf_path = os.path.join(OUTPUT_DIR, pdf_file)
 print(f"✅ HTML saved to: {html_path}")
 
-#pdfkit.from_file(html_path, pdf_path)
+pdfkit.from_file(html_path, pdf_path)
 print(f"✅ PDF saved to: {pdf_path}")
