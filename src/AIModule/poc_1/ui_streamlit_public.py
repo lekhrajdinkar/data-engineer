4a9ce8fac3080765346d0ee3e0b6b130dadc55c7
lekhrajdinkar <lekhrajdinkar@gmail.com>
2025-06-29 15:26:48 -0700

    poc-1 : done 😁

--- src/AIModule/poc_1/ui_streamlit_public.py
+++ src/AIModule/poc_1/ui_streamlit_public.py
@@ -1,7 +1,3 @@
-import sys
-import os
-# Add project root (one level above src) to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 import boto3
 import json
 
